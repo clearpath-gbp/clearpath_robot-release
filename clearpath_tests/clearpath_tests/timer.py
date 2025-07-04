b2d94ddc5d8480721cd3225a3ad49013b0f5abab
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2025-07-04 11:53:08 -0400

    Rebase from 'upstream'

--- clearpath_tests/clearpath_tests/timer.py
+++ clearpath_tests/clearpath_tests/timer.py
@@ -26,35 +26,42 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-from glob import glob
-import os
+import threading
+import time
 
-from setuptools import find_packages, setup
+from rclpy.duration import Duration
+from rclpy.node import Node
 
-package_name = 'clearpath_tests'
 
-setup(
-    name=package_name,
-    version='2.6.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        (os.path.join('share', package_name), ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(
-            os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='Chris Iverach-Brereton',
-    maintainer_email='civerachb@clearpathrobotics.com',
-    description='Testing scripts for Clearpath robots',
-    license='BSD',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'all_tests = clearpath_tests.all_tests:main',
-        ],
-    },
-)
+class Timeout:
+    """
+    Reusable class for implementing basic timeouts during ROS operation.
+
+    Starts a background thread that spins until the time has elapsed.
+    """
+
+    def __init__(self, node: Node, duration: float):
+        self.node = node
+        self.duration = Duration(seconds=duration)
+        self.__abort_signal = False
+        self.__is_elapsed = False
+        self.time_thread = threading.Thread(
+            target=self.run_timer
+        )
+        self.time_thread.start()
+
+    @property
+    def elapsed(self):
+        return self.__is_elapsed
+
+    def abort(self):
+        self.__abort_signal = True
+
+    def run_timer(self):
+        start_time = self.node.get_clock().now()
+        while (
+            self.node.get_clock().now() - start_time < self.duration
+            and not self.__abort_signal
+        ):
+            time.sleep(0.01)
+        self.__is_elapsed = True
