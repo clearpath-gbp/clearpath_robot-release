5b9d43f7a578980c051f0546e39a6908887e4e65
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2026-04-14 21:15:59 -0400

    Rebase from 'release/jazzy/clearpath_hardware_interfaces'

--- src/lynx/hardware.cpp
+++ src/lynx/hardware.cpp
@@ -102,7 +102,7 @@ void LynxHardware::writeCommandsToHardware()
  * @brief Pull latest speed and travel measurements from MCU,
  * and store in joint structure for ROS controls
 */
-void LynxHardware::updateJointsFromHardware(const rclcpp::Duration & period)
+void LynxHardware::updateJointsFromHardware(const rclcpp::Duration & /*period*/)
 {
   rclcpp::spin_some(node_);
 
@@ -127,10 +127,10 @@ void LynxHardware::updateJointsFromHardware(const rclcpp::Duration & period)
 /**
  * @brief Get hardware information from robot description
 */
-hardware_interface::CallbackReturn LynxHardware::getHardwareInfo(const hardware_interface::HardwareInfo & info)
+hardware_interface::CallbackReturn LynxHardware::getHardwareInfo(const hardware_interface::HardwareComponentInterfaceParams & params)
 {
   // Get info from URDF
-  if (hardware_interface::SystemInterface::on_init(info) != hardware_interface::CallbackReturn::SUCCESS)
+  if (hardware_interface::SystemInterface::on_init(params) != hardware_interface::CallbackReturn::SUCCESS)
   {
     return hardware_interface::CallbackReturn::ERROR;
   }
@@ -218,11 +218,11 @@ hardware_interface::CallbackReturn LynxHardware::validateJoints()
 /**
  * @brief Initialization
 */
-hardware_interface::CallbackReturn LynxHardware::on_init(const hardware_interface::HardwareInfo & info)
+hardware_interface::CallbackReturn LynxHardware::on_init(const hardware_interface::HardwareComponentInterfaceParams & params)
 {
   hardware_interface::CallbackReturn ret;
   // Get Hardware name and joints
-  ret = getHardwareInfo(info);
+  ret = getHardwareInfo(params);
 
   if (ret != hardware_interface::CallbackReturn::SUCCESS)
   {
